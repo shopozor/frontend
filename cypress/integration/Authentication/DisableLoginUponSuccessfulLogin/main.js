da10d822f555f0e949816b9b8f63c9dc696272e6
Laurent Michel <laurent.michel@softozor.ch>
2019-02-17 15:33:32 +0100

    Init test code

---
+++ cypress/integration/Authentication/DisableLoginUponSuccessfulLogin/main.js
@@ -0,0 +1,14 @@
+Given('un Consommateur identifié', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When("il navigue vers l'interface d'identification", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then("il est redirigé vers la page d'accueil", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
