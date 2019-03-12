e591819785328d9423e4f51a3181c7a15c9135e4
Laurent Michel <laurent.michel@softozor.ch>
2019-03-12 17:04:29 +0100

    Adapt and complete customer registration spec and test code

--- cypress/integration/Authentication/RegisterCustomer.feature
+++ cypress/integration/Authentication/RegisterCustomer.feature
@@ -52,8 +52,7 @@ Fonctionnalité: Enregistrer un nouveau client
 
     L'adresse ainsi que les nom et prénom du client seront demandés plus tard si nécessaire.  
 
-    Un client est connu du Shopozor si, et seulement si, son e-mail est inclus dans 
-    sa base de données.
+    Un client est inconnu si, et seulement si, son e-mail n'existe pas dans la base de données.
 
     Lorsqu'un client inconnu entre son e-mail dans le formulaire d'enregistrement
     Et accepte la politique relative aux cookies
@@ -61,52 +60,91 @@ Fonctionnalité: Enregistrer un nouveau client
     Et fait la demande d'enregistrement
     Alors il obtient un message stipulant qu'un e-mail lui a été transmis
 
-  Scénario: Un utilisateur s'enregistre avec un e-mail déjà connu
+  Scénario: Un utilisateur s'enregistre avec l'e-mail d'un compte actif
 
     Le Shopozor va communiquer qu'un e-mail est transmis à ce client qui n'est pas nouveau. 
     L'e-mail va simplement notifier le client que quelqu'un est probablement en train d'utiliser 
     son e-mail à des fins malveillantes. En plus de ça, le système va notifier Softozor du 
     problème potentiel. 
 
-    Lorsqu'un utilisateur enregistré fait une demande d'enregistrement
+    Lorsqu'un utilisateur entre l'e-mail d'un compte actif dans le formulaire d'enregistrement
+    Et accepte la politique relative aux cookies
+    Et les conditions générales d'utilisation 
+    Et fait la demande d'enregistrement
+    Alors il obtient un message stipulant qu'un e-mail lui a été transmis
+
+  Scénario: Un utilisateur s'enregistre avec l'e-mail d'un compte inactif
+
+    Si l'utilisateur a déjà fait une demande d'enregistrement et qu'il n'a pas activé son compte 
+    dans les temps, alors son compte existe toujours mais il est inactif. Dans ce cas, l'utilisateur 
+    peut tout à fait refaire une demande d'enregistrement pour obtenir un nouveau lien d'activation 
+    de compte. 
+
+    Lorsqu'un utilisateur entre l'e-mail d'un compte inactif dans le formulaire d'enregistrement
+    Et accepte la politique relative aux cookies
+    Et les conditions générales d'utilisation 
+    Et fait la demande d'enregistrement
     Alors il obtient un message stipulant qu'un e-mail lui a été transmis
 
   Scénario: Le client accède à l'interface de définition du mot de passe
   
-    Lorsqu'un client consulte son lien de confirmation de création de compte
+    Que le lien d'activation de compte soit valide ou pas, l'utilisateur peut 
+    accéder au formulaire de définition de mot de passe. C'est au moment où il 
+    confirme son mot de passe que la validité de son lien est vérifiée. 
+
+    Lorsqu'un client consulte son lien d'activation de compte
     Alors il est redirigé vers une interface où il peut définir son mot de passe
+    Mais le bouton d'activation du compte est désactivé
+
+  Scénario: Le bouton d'activation de compte s'active
+
+    Le mot de passe doit satisfaire à la politique relative aux mots de passe. 
+
+    Etant donné un client qui consulte son lien d'activation de compte
+    Lorsqu'il tape un mot de passe conforme dans le champ correspondant
+    Alors le bouton d'activation du compte est activé 
+
+  Scénario: Le client passe d'un mot de passe conforme à non conforme
+
+    Le client ne doit pas pouvoir définir un mot de passe non conforme à 
+    la politique relative aux mots de passe.
+
+    Etant donné un client qui consulte son lien d'activation de compte
+    Et qui a tapé un mot de passe conforme dans le champ correspondant
+    Lorsqu'il rend son mot de passe non conforme 
+    Alors le bouton d'activation du compte est désactivé 
 
   @not-e2e
-  Scénario: Le client définit son mot de passe à temps
+  Scénario: Le client définit un mot de passe conforme à temps
 
     # Tester ce scénario "end-to-end" pourrait être techniquement possible. Il faudrait 
     # mettre en place mailtrap et consulter cette boîte mail durant le test. Dans une 
     # première phase, nous ne considérons pas ce scénario durant les test e2e.
 
-    Etant donné un client qui consulte son lien de confirmation de création de compte
-    Lorsqu'il définit son mot de passe dans les temps
-    Alors il obtient un message stipulant que la création du compte a été effectuée avec succès
+    Etant donné un client qui consulte son lien d'activation de compte
+    Lorsqu'il définit un mot de passe conforme dans les temps
+    Alors il obtient un message stipulant que l'activation du compte a été effectuée avec succès
     Mais il n'est pas identifié
 
   @not-e2e
-  Scénario: Le client confirme la création de compte une deuxième fois
+  Scénario: Le client définit un mot de passe conforme une deuxième fois
   
     # Tester ce scénario "end-to-end" pourrait être techniquement possible. Il faudrait 
     # mettre en place mailtrap et consulter cette boîte mail durant le test. Dans une 
     # première phase, nous ne considérons pas ce scénario durant les test e2e.
 
-    Etant donné un client qui a consulté son lien de confirmation de création de compte
-    Et qui a déjà défini son mot de passe dans les temps
-    Lorsqu'il définit son mot de passe une deuxième fois
+    Etant donné un client qui a consulté son lien d'activation de compte
+    Et qui a déjà défini un mot de passe conforme dans les temps
+    Lorsqu'il définit un mot de passe conforme une deuxième fois
     Alors il obtient un message stipulant que le lien a expiré
 
   @not-e2e
-  Scénario: Le client confirme son adresse e-mail trop tard
+  Scénario: Le client définit un mot de passe conforme trop tard
 
     # Tester ce scénario "end-to-end" n'est pas raisonnable car il faudrait en principe 
     # attendre suffisamment longtemps pour que le lien expire. Il est possible de faire 
     # avancer le temps durant les tests côté serveur mais pas durant les tests e2e. 
 
-    Etant donné un client qui consulte son lien de confirmation de création de compte 
-    Lorsqu'il définit son mot de passe trop tard
+    Etant donné un client qui consulte son lien d'activation de compte 
+    Lorsqu'il définit un mot de passe conforme trop tard
     Alors il obtient un message stipulant que le lien a expiré
--- cypress/integration/Authentication/RegisterCustomer/main.js
+++ cypress/integration/Authentication/RegisterCustomer/main.js
@@ -5,10 +5,12 @@ function accessRegistrationInterface() {
   cy.visit('/register')
 }
 
+// OK
 Given("^un utilisateur (?:ayant accédé|accède) au formulaire d'enregistrement$", function () {
   accessRegistrationInterface()
 });
 
+// OK
 When("^un client inconnu entre son e-mail dans le formulaire d'enregistrement$", function () {
   injectResponseFixtureIfFaked('Authentication/RegisterCustomer/Responses/SuccessfulCustomerCreation')
   accessRegistrationInterface()
@@ -18,33 +20,39 @@ When("^un client inconnu entre son e-mail dans le formulaire d'enregistrement$",
     .then(user => fillUserRegistrationGui(user.email))
 });
 
+// OK
 When("^(?:il|qui a)?\s?accept(?:e|é) la politique relative aux cookies$", function () {
   // 1. check the cookies policy checkbox 
   return 'pending'
 });
 
+// OK
 When("^les conditions générales d'utilisation$", function () {
   // 1. check the general conditions checkbox
   return 'pending'
 });
 
+// OK
 When("^il n'accepte plus la politique relative aux cookies$", function () {
   // 1. uncheck the cookies policy checkbox
   return 'pending'
 })
 
+// OK
 When("^il n'accepte plus les conditions générales$", function () {
   // 1. uncheck the general conditions checkbox
   return 'pending'
 })
 
+// OK
 When("^fait la demande d'enregistrement$", function () {
   // 1. assert that the register button is enabled
   // 2. click the register button
   return 'pending'
 });
 
-When("^un utilisateur enregistré fait une demande d'enregistrement$", function () {
+// OK
+When("^un utilisateur entre l'e-mail d'un compte actif dans le formulaire d'enregistrement$", function () {
   injectResponseFixtureIfFaked('Authentication/RegisterCustomer/Responses/SuccessfulCustomerCreation')
   accessRegistrationInterface()
   cy.fixture('Authentication/Credentials/Consommateur')
@@ -55,12 +63,38 @@ When("^un utilisateur enregistré fait une demande d'enregistrement$", function 
   return 'pending'
 });
 
-When("^(?:un|le) client (?:qui)?\s?(?:consulte|a consulté) son lien de confirmation de création de compte$", function () {
+// OK
+When("^un utilisateur entre l'e-mail d'un compte inactif dans le formulaire d'enregistrement$", function () {
+  injectResponseFixtureIfFaked('Authentication/RegisterCustomer/Responses/SuccessfulCustomerCreation')
+  accessRegistrationInterface()
+  cy.fixture('Authentication/Credentials/InactiveCustomer')
+    // you need to provide the fillUserRegistrationGui
+    // that method must not be in the common folder
+    .then(user => fillUserRegistrationGui(user.email))
+  // TODO: click the registration button
+  return 'pending'
+});
+
+// OK
+When("^(?:un|le) client (?:qui)?\s?(?:consulte|a consulté) son lien d'activation de compte$", function () {
   // TODO: visit /activate/uidb64/token <-- get from fixtures
   cy.visit('/activate/uidb64/token')
 });
 
-When("^(?:il|qui a déjà) définit? son mot de passe dans les temps$", function () {
+// TODO: refactor the following two steps so that they call the same function (they indeed write the password in the field)
+// OK
+When("^(?:qui a|il) tap(?:e|é) un mot de passe conforme dans le champ correspondant$", function () {
+  injectResponseFixtureIfFaked('Authentication/RegisterCustomer/Responses/SuccessfulAccountConfirmation')
+  cy.fixture('Authentication/Credentials/NewCustomer')
+    // you need to provide the fillUserPasswordGui
+    // that method must be in the common folder, as the password can 
+    // be reset in both frontend applications
+    .then(user => fillUserPasswordGui(user.email))
+  // TODO: click the set password button
+})
+
+// OK
+When("^(?:il|qui a déjà) définit? un mot de passe conforme dans les temps$", function () {
   injectResponseFixtureIfFaked('Authentication/RegisterCustomer/Responses/SuccessfulAccountConfirmation')
   cy.fixture('Authentication/Credentials/NewCustomer')
     // you need to provide the fillUserPasswordGui
@@ -70,7 +104,13 @@ When("^(?:il|qui a déjà) définit? son mot de passe dans les temps$", function
   // TODO: click the set password button
 });
 
-function newCustomerDefinesPasswordWhenLinkExpired() {
+// OK
+When("^il rend son mot de passe non conforme$", function () {
+  // 1. empty password field
+  return 'pending'
+})
+
+function newCustomerDefinesValidPasswordWhenLinkExpired() {
   injectResponseFixtureIfFaked('Authentication/RegisterCustomer/Responses/ExpiredAccountConfirmationLink')
   cy.fixture('Authentication/Credentials/NewCustomer')
     // you need to provide the fillUserPasswordGui
@@ -80,20 +120,22 @@ function newCustomerDefinesPasswordWhenLinkExpired() {
   // TODO: click the set password button
 }
 
-When("^il définit son mot de passe trop tard$", function () {
-  newCustomerDefinesPasswordWhenLinkExpired()
+When("^il définit un mot de passe conforme trop tard$", function () {
+  newCustomerDefinesValidPasswordWhenLinkExpired()
 });
 
-When("^il définit son mot de passe une deuxième fois$", function () {
-  newCustomerDefinesPasswordWhenLinkExpired()
+When("^il définit un mot de passe conforme une deuxième fois$", function () {
+  newCustomerDefinesValidPasswordWhenLinkExpired()
 })
 
+// OK
 Then("^il peut consulter la politique relative aux cookies$", function () {
   // assert that the link to the cookies policy exists
   // assert that the link to the cookies policy is working
   return 'pending'
 })
 
+// OK
 Then("^il peut consulter les conditions générales$", function () {
   // assert that the link to the general conditions exists
   // assert that the link to the general conditions is working
@@ -101,28 +143,39 @@ Then("^il peut consulter les conditions générales$", function () {
 })
 
 // TODO: possibly group the following two Then steps together
+// OK
 Then("^la création de compte est possible$", function () {
   // 1. assert that the registration button is enabled
   return 'pending'
 });
 
+// OK
 Then("^la création de compte n'est plus possible$", function () {
   // 1. assert that the registration button is disabled
   return 'pending'
 })
 
+// OK
 Then("^il obtient un message stipulant qu'un e-mail lui a été transmis$", function () {
   // if the graphql query was successful, then the frontend receives the SuccessfulCustomerCreation response, i.e. an answer with no data and no errors
   // upon clicking the registration button, you need to check that the message about the e-mail is displayed
   return 'pending'
 });
 
+// OK
 Then("^il est redirigé vers une interface où il peut définir son mot de passe$", function () {
   // 1. assert that there is an input field for a password
   return 'pending'
 });
 
-Then("^il obtient un message stipulant que la création du compte a été effectuée avec succès$", function () {
+// TODO: create a custom type parsing "désactivé" and "activé" which are interpreted as false and true respectively
+// upon parsing, the following step can handle both cases
+Then("^le bouton d'activation du compte est désactivé$", function () {
+  // 1. assert that the form's send button is disabled / enabled
+  return 'pending'
+})
+
+Then("^il obtient un message stipulant que l'activation du compte a été effectuée avec succès$", function () {
   // the graphql query returns the SuccessfulAccountConfirmation response, i.e. 
   // an answer with no data and no errors
   // TODO: upon clicking the set password button, you need to check that the corresponding message is displayed
