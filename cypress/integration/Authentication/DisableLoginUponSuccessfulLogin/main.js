6c929c2ef7652126dc8c7b2f6678acf3f1b4451f
Laurent Michel <laurent.michel@ruag.com>
2019-05-16 11:27:07 +0200

    call fixture injection before each login

--- cypress/integration/Authentication/DisableLoginUponSuccessfulLogin/main.js
+++ cypress/integration/Authentication/DisableLoginUponSuccessfulLogin/main.js
@@ -4,6 +4,7 @@ import '../../../../common/cypress/integration/Authentication/common/PersonaType
 import { login } from '../../../../common/cypress/integration/Authentication/common/Helpers'
 
 Given("un {PersonaType} identifié", function (persona) {
+  injectResponseFixtureIfFaked(`Authentication/LogConsumerIn/Responses/${persona}`)
   login(persona)
 });
 
@@ -13,5 +14,4 @@ When("il navigue vers l'interface d'identification", function () {
 
 Then("il est redirigé vers la page d'accueil", function () {
   cy.url().should('not.include', '/login')
-  // TODO: verify that the user is redirected to the home page
 });
