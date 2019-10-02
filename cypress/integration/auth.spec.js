76ae98d8b9ae6373764ed1669f76e9cdcba712ef
Laurent Michel <laurent.michel@ruag.com>
2019-10-02 17:18:02 +0200

    * update common submodule * group all integration tests related to auth in the same spec file

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -1,9 +1,103 @@
 import {
-  getTokenCookie
+  checkIfLinkIsActive,
+  connectWithUserCredentialsViaGui,
+  getTokenCookie,
+  login,
+  navigateTo
 } from '../../common/cypress/Authentication/Helpers'
+import TokenHandler from '../../common/cypress/Authentication/TokenHandler'
 import types from '../../common/types'
 
-describe('Sign Consumer up', function(){
+describe('Consumer authentication', function(){
+
+  context('Log consumer in', function() {
+    
+    const email = 'test@example.com'
+    const password = 'password'
+
+    beforeEach(() => getTokenCookie().should('not.exist'))
+
+    // TODO: the same test needs to be run on the management-frontend side; maybe we could just 
+    // TODO: put this test into the common repo and import it somehow
+    it('redirects to home page if identified Consumer browses /login', function () {
+      // Given
+      cy.stubServer(`Authentication/LogConsumerIn/Consommateur`)
+      login(email, password)
+
+      // When
+      cy.visit('/login')
+
+      // Then
+      cy.url().should('not.include', '/login')
+    })
+
+    it('pops up an error message if a Consumer logs in with invalid e-mail and password', function() {
+      // Given
+      cy.stubServer('Authentication/LogConsumerIn/WrongCredentials')
+
+      // When
+      cy.visit('/login')
+      connectWithUserCredentialsViaGui(email, password)
+
+      // Then
+      cy.get('@graphql').then(() => {
+        cy.get('.incorrectIdentifiers')
+          .should('be.visible')
+      })
+    })
+
+    it('pops up an error message if a registered Consumer logs in with an invalid password', function() {
+      // Given
+      cy.stubServer('Authentication/LogConsumerIn/WrongCredentials')
+
+      // When
+      cy.visit('/login')
+      connectWithUserCredentialsViaGui(email, password)
+
+      // Then
+      cy.get('@graphql').then(() => {
+        cy.get('.incorrectIdentifiers')
+          .should('be.visible')
+      })
+    })
+
+    it('opens a session if the Consumer provides the correct credentials', function() {
+      // Given
+      cy.stubServer(`Authentication/LogConsumerIn/Consommateur`)
+
+      // When
+      cy.visit('/login')
+      connectWithUserCredentialsViaGui(email, password)
+
+      // Then
+      cy.get('@graphql').then(() => {
+        getTokenCookie().should('exist')
+      })
+    })
+  })
+
+  context('Log consumer out', function() {
+    
+    const email = 'test@example.com'
+    const password = 'password'
+
+    it('forgets about the token and redirects to /', function () {
+      // Given
+      cy.stubServer(`Authentication/LogConsumerIn/Consommateur`)
+      login(email, password)
+
+      // When
+      navigateTo(types.links.LOGOUT)
+
+      // Then
+      const tokenHandler = new TokenHandler
+      tokenHandler.getNullToken().then(() => {
+        getTokenCookie().should('not.exist')
+      })
+      cy.location('pathname').should('eq', '/')
+      checkIfLinkIsActive(types.links.HOME)
+    })
+  })
 
   context('Consumer registration', function() {
     
@@ -118,4 +212,5 @@ describe('Sign Consumer up', function(){
       cy.get('[id="errorActivationLinkExpired"]').should('exist')
     })
   })
+
 })
