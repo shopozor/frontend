54da7884db157130457ffc00d3b3f0274b397e91
comteharbour <florian.pittet@softozor.ch>
2019-11-01 12:53:05 +0100

    producer requests his products fixture more examples

--- src/store/fixturesExamples/producerRequestsHisProducts.js
+++ src/store/fixturesExamples/producerRequestsHisProducts.js
@@ -5,6 +5,8 @@
 import types from '../../types'
 
 export const products = {
+  // the first product shows all the data I might need
+  // the following products are examples that could be used
   productId1: { // should be ['productId1'], but eslint does not agree
     productId: 'productId1',
     producerId: 'producerId',
@@ -27,14 +29,14 @@ export const products = {
     defaultGrossConsumerPrice: 10, // can be undefined
     defaultUnit: types.units.KG, // can be undefined
     variants: {
-      variantId1: { // should be ['variantId1'], but eslint does not agree
-        variantId: 'variantId1',
+      variantId1_1: { // should be ['variantId1'], but eslint does not agree
+        variantId: 'variantId1_1',
         productId: 'productId1',
         ordersId: [
-          'orderId1',
-          'orderId2',
-          'orderId3',
-          'orderId4'
+          'orderId1_1_1',
+          'orderId1_1_2',
+          'orderId1_1_3',
+          'orderId1_1_4'
         ],
         state: types.variantState.VISIBLE,
         description: {
@@ -57,6 +59,156 @@ export const products = {
             unPaid: 1,
             paid: 2,
             delivered: 51
+          }
+        }
+      }
+    }
+  },
+  productId2: {
+    productId: 'productId2',
+    producerId: 'producerId',
+    state: types.productState.VISIBLE,
+    // ordersSummary can me generated through an "orders" request
+    // it may not be useful
+    ordersSummary: {
+      unPaid: 2,
+      paid: 12,
+      delivered: 81
+    },
+    image: 'imageUrl or imageData',
+    name: 'carottes',
+    description: 'des carottes faites avec amour et passion dans de la vraie terre bien de chez nous !',
+    conservationMode: types.conservationModes.BASEMENT,
+    conservationDays: 100,
+    categories: [types.categories.VEGETABLES],
+    defaultPriceMode: types.priceModes.AUTO_PRICE,
+    defaultGrossConsumerPrice: 3,
+    defaultUnit: types.units.KG,
+    variants: {
+      variantId2_1: {
+        variantId: 'variantId2_1',
+        productId: 'productId2',
+        ordersId: [
+          'orderId2_1_1',
+          'orderId2_1_2',
+          'orderId2_1_3'
+        ],
+        state: types.variantState.VISIBLE,
+        description: {
+          measure: 500,
+          measureUnit: types.units.GR
+        },
+        priceMode: types.priceModes.AUTO_PRICE,
+        grossConsumerPrice: 1.50,
+        stock: {
+          mode: types.stockModes.AMOUNT,
+          amount: 97,
+          // ordersSummary can me generated through an "orders" request
+          // it may not be useful
+          ordersSummary: {
+            unPaid: 1,
+            paid: 2,
+            delivered: 51
+          }
+        }
+      },
+      variantId2_2: {
+        variantId: 'variantId2_2',
+        productId: 'productId2',
+        ordersId: [
+          'orderId2_2_1',
+          'orderId2_2_2',
+          'orderId2_2_3'
+        ],
+        state: types.variantState.VISIBLE,
+        description: {
+          measure: 2,
+          measureUnit: types.units.KG
+        },
+        priceMode: types.priceModes.AUTO_PRICE,
+        grossConsumerPrice: 6,
+        stock: {
+          mode: types.stockModes.AMOUNT,
+          amount: 40,
+          // ordersSummary can me generated through an "orders" request
+          // it may not be useful
+          ordersSummary: {
+            unPaid: 1,
+            paid: 10,
+            delivered: 30
+          }
+        }
+      }
+    }
+  },
+  productId3: {
+    productId: 'productId3',
+    producerId: 'producerId',
+    state: types.productState.VISIBLE,
+    // ordersSummary can me generated through an "orders" request
+    // it may not be useful
+    ordersSummary: {
+      unPaid: 2,
+      paid: 10,
+      delivered: 153
+    },
+    image: 'imageUrl or imageData',
+    name: 'savon',
+    description: 'savon à l\'huile de colza et au fécule de pomme de terre',
+    productVAT: 0.078,
+    conservationMode: types.conservationModes.BASEMENT,
+    conservationDays: 730,
+    categories: [types.categories.HEALTH],
+    variants: {
+      variantId3_1: {
+        variantId: 'variantI3_1',
+        productId: 'productId3',
+        ordersId: [
+          'orderId3_1_1',
+          'orderId3_1_2',
+          'orderId3_1_3',
+          'orderId3_1_4'
+        ],
+        state: types.variantState.VISIBLE,
+        description: {
+          name: 'savon à la lavande'
+        },
+        priceMode: types.priceModes.FREE,
+        grossConsumerPrice: 5.50,
+        stock: {
+          mode: types.stockModes.AMOUNT,
+          amount: 50,
+          // ordersSummary can me generated through an "orders" request
+          // it may not be useful
+          ordersSummary: {
+            unPaid: 1,
+            paid: 2,
+            delivered: 51
+          }
+        }
+      },
+      variantId3_2: {
+        variantId: 'variantI3_2',
+        productId: 'productId3',
+        ordersId: [
+          'orderId3_2_1',
+          'orderId3_2_2'
+        ],
+        state: types.variantState.VISIBLE,
+        description: {
+          name: 'savon au cumin'
+        },
+        priceMode: types.priceModes.FREE,
+        grossConsumerPrice: 6.40,
+        stock: {
+          mode: types.stockModes.AMOUNT,
+          amount: 40,
+          // ordersSummary can me generated through an "orders" request
+          // it may not be useful
+          ordersSummary: {
+            unPaid: 1,
+            paid: 8,
+            delivered: 102
           }
         }
       }
