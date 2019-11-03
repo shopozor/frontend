e3f7c5bff9e8426c9811216b0f71109ca0d69666
comteharbour <florian.pittet@softozor.ch>
2019-11-03 13:01:18 +0100

    VaruiantDescriptionFree test

---
+++ src/components/Products/Edit/Variants/VariantDescription/__tests__/VariantDescriptionFree.spec.js
@@ -0,0 +1,45 @@
+import VariantDescriptionFree from '../VariantDescriptionFree'
+import { mountQuasar } from '../../../../../../../test/utils'
+import types from '../../../../../../types'
+
+const variantId = 'testId'
+
+const name = 'testName'
+
+const store = {
+  getters: {
+    editedVariantName: () => () => name,
+    pendingOrdersOfVariantSummary: () => () => {
+      return {
+        paid: {
+          amount: 0
+        },
+        notPaid: {
+          amount: 0
+        }
+      }
+    }
+  },
+  actions: {
+    updateEditedVariantName: jest.fn()
+  }
+}
+
+const wrapper = mountQuasar(VariantDescriptionFree, {
+  shallow: true,
+  propsData: {
+    variantId
+  },
+  store
+})
+
+const qInput = wrapper.find({ name: 'QInput' })
+
+test('has a q-input with value editedVariantName', () => {
+  expect(qInput.vm.value).toEqual(name)
+})
+
+test('triggers updateEditedVariantName when unit-field emits input', () => {
+  qInput.vm.$emit('input', 'newName')
+  expect(store.actions.updateEditedVariantName).toHaveBeenCalled()
+})
